889b22c052514ea2ce99f0ed41e1727541978501
Miles <59800778+MilesHoover@users.noreply.github.com>
2020-01-22 18:41:58 -0500

    page 152 update

--- JS Projects/JS/basic_javascript_9.js
+++ JS Projects/JS/basic_javascript_9.js
@@ -40,3 +40,17 @@ function countdown() {
         }
     tick();
 }
+
+function countdown() {
+  var seconds = document.getElementById("seconds").value;
+
+  function tick() {
+    seconds = seconds - 1;
+    timer.innerHTML = seconds;
+    setTimeout(tick, 1000);
+    if (seconds == -1) {
+      alert("Time's up!");
+    }
+  }
+  tick();
+}
